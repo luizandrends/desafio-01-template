a538ca61e23b1c08324a42c528a57767523a7514
Luiz Andre <luizxd234@gmail.com>
2021-03-09 15:13:31 -0300

    feat(todos): Creating and listing todos

--- src/index.js
+++ src/index.js
@@ -15,11 +15,11 @@ function checksExistsUserAccount(request, response, next) {
 
   const findUserByUsername = users.find(user => user.username === username);
 
-  if(findUserByUsername) {
-    return response.status(400).json({ error: 'User already exists' });
+  if(!findUserByUsername) {
+    return response.status(400).json({ error: 'Unexistent user' });
   }
 
-  return next;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -44,11 +44,32 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const findUserByUsername = users.find(user => user.username === username);
+
+  const { todos } = findUserByUsername;
+
+  return response.json(todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request.headers;
+
+  const findUserIndex = users.findIndex(user => user.username === username);
+
+  const createTodoData = { 
+    id: uuidv4(),
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  };
+
+  users[findUserIndex].todos.push(createTodoData);
+
+  return response.status(201).json(createTodoData);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
